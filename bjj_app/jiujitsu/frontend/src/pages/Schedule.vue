6ae59df86b230458673dce8b28c2e943aef27701
cescalante-dev <carlos.dev.escalante@gmail.com>
2020-04-30 20:46:25 -0600

    Added Calendar view

--- bjj_app/jiujitsu/frontend/src/pages/Schedule.vue
+++ bjj_app/jiujitsu/frontend/src/pages/Schedule.vue
@@ -16,9 +16,8 @@
         <q-calendar
             ref="calendar"
             v-model="selectedDate"
-            view="month"
+            view="week-agenda"
             locale="es-ES"
-            show-day-of-year-label
             short-weekday-label
             show-work-weeks
             animated
@@ -27,8 +26,27 @@
             transition-next="slide-left"
             :resources="resources">
 
-            <template #scheduler-resource-day="{ day, index, resource }">
-                <q-btn class="fit"><span class="ellipsis" style="font-size: 10px;">{{ resource.label }}:{{ day.day }}</span></q-btn>
+        
+            <template #day-body="day">
+                <template v-for="(agenda) in getAgenda(day)">
+                <div
+                    :key="day.date + agenda.time"
+                    :label="agenda.time"
+                    class="justify-start q-ma-sm shadow-5 bg-grey-2"
+                >
+                    <div v-if="agenda.avatar" class="row justify-center" style="margin-top: 30px; width: 100%;">
+                    <q-avatar style="margin-top: -25px; margin-bottom: 10px; font-size: 60px; max-height: 50px;">
+                        <img :src="agenda.avatar" style="border: #9e9e9e solid 5px;">
+                    </q-avatar>
+                    </div>
+                    <div class="col-12 q-px-sm">
+                    <strong>{{ agenda.time }}</strong>
+                    </div>
+                    <div v-if="agenda.desc" class="col-12 q-px-sm" style="font-size: 10px;">
+                    {{ agenda.desc }}
+                    </div>
+                </div>
+                </template>
             </template>
 
         </q-calendar>
@@ -45,8 +63,20 @@ export default {
         QCalendar
     },
 
+    computed: {
+        currentMonth() {
+            return this.$refs.calendar
+        }
+    },
+
     methods: {
+        getAgenda (day) {
+        return this.agenda[parseInt(day.weekday, 10)]
+        },
+
+
         calendarNext () {
+            console.log(this.$refs.calendar)
         this.$refs.calendar.next()
         },
         calendarPrev () {
@@ -76,14 +106,131 @@ export default {
             dragging: false, // used for drag-and-drop
             ignoreNextSwipe: false, // used for drag-and-drop
             resources: [
-                { label: 'John' },
-                { label: 'Mary' },
-                { label: 'Susan' },
+                { label: 'BJJ Gi' },
+                { label: 'BJJ No Gi' },
+                { label: 'Físico' },
                 { label: 'Olivia' },
                 { label: 'Board Room' },
                 { label: 'Room-1' },
                 { label: 'Room-2' }
-            ]
+            ],
+
+            agenda: {
+        // value represents day of the week
+        1: [
+          {
+            time: '08:30',
+            avatar: 'https://cdn.quasar.dev/img/avatar.png',
+            desc: 'Meeting with HR'
+          },
+          {
+            time: '10:00',
+            avatar: 'https://cdn.quasar.dev/img/avatar1.jpg',
+            desc: 'Meeting with Karen'
+          }
+        ],
+        2: [
+          {
+            time: '08:00',
+            avatar: 'https://cdn.quasar.dev/img/boy-avatar.png',
+            desc: 'Meeting with CEO'
+          },
+          {
+            time: '11:30',
+            avatar: 'https://cdn.quasar.dev/img/avatar2.jpg',
+            desc: 'Meeting with Alisha'
+          },
+          {
+            time: '17:00',
+            avatar: 'https://cdn.quasar.dev/img/avatar3.jpg',
+            desc: 'Meeting with Sarah'
+          }
+        ],
+        3: [
+          {
+            time: '08:00',
+            desc: 'Stand-up SCRUM',
+            avatar: 'https://cdn.quasar.dev/img/material.png'
+          },
+          {
+            time: '09:00',
+            avatar: 'https://cdn.quasar.dev/img/boy-avatar.png'
+          },
+          {
+            time: '10:00',
+            desc: 'Sprint planning',
+            avatar: 'https://cdn.quasar.dev/img/material.png'
+          },
+          {
+            time: '13:00',
+            avatar: 'https://cdn.quasar.dev/img/avatar1.jpg'
+          }
+        ],
+        4: [
+          {
+            time: '09:00',
+            avatar: 'https://cdn.quasar.dev/img/avatar3.jpg'
+          },
+          {
+            time: '10:00',
+            avatar: 'https://cdn.quasar.dev/img/avatar2.jpg'
+          },
+          {
+            time: '13:00',
+            avatar: 'https://cdn.quasar.dev/img/material.png'
+          }
+        ],
+        5: [
+          {
+            time: '08:00',
+            avatar: 'https://cdn.quasar.dev/img/boy-avatar.png'
+          },
+          {
+            time: '09:00',
+            avatar: 'https://cdn.quasar.dev/img/avatar2.jpg'
+          },
+          {
+            time: '09:30',
+            avatar: 'https://cdn.quasar.dev/img/avatar4.jpg'
+          },
+          {
+            time: '10:00',
+            avatar: 'https://cdn.quasar.dev/img/avatar5.jpg'
+          },
+          {
+            time: '11:30',
+            avatar: 'https://cdn.quasar.dev/img/material.png'
+          },
+          {
+            time: '13:00',
+            avatar: 'https://cdn.quasar.dev/img/avatar6.jpg'
+          },
+          {
+            time: '13:30',
+            avatar: 'https://cdn.quasar.dev/img/avatar3.jpg'
+          },
+          {
+            time: '14:00',
+            avatar: 'https://cdn.quasar.dev/img/linux-avatar.png'
+          },
+          {
+            time: '14:30',
+            avatar: 'https://cdn.quasar.dev/img/avatar.png'
+          },
+          {
+            time: '15:00',
+            avatar: 'https://cdn.quasar.dev/img/boy-avatar.png'
+          },
+          {
+            time: '15:30',
+            avatar: 'https://cdn.quasar.dev/img/avatar2.jpg'
+          },
+          {
+            time: '16:00',
+            avatar: 'https://cdn.quasar.dev/img/avatar6.jpg'
+          }
+        ]
+      }
         }
     }
 }
